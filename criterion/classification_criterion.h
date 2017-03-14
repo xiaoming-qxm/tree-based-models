939f1b0677cb76786d2e6582db930701329c1b51
Qin Xiaoming <simon.q.chin@gmail.com>
2017-03-14 19:15:44 +0800

    Add files via upload

--- criterion/classification_criterion.h
+++ criterion/classification_criterion.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 The Authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file, See the AUTHORS file for names of contributors.
+
 #ifndef CLASSIFICATION_CRITERION_
 #define CLASSIFICATION_CRITERION_
 
--- criterion/regression_criterion.h
+++ criterion/regression_criterion.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 The Authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file, See the AUTHORS file for names of contributors.
+
 #ifndef REGRESSION_CRITERION_H_
 #define REGRESSION_CRITERION_H_
 
