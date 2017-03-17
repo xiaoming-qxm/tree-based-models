1c2bf445be193208633184aa27021d15b76478a7
daoliker <simon.q.chin@gmail.com>
2017-03-17 13:25:20 +0800

    remove log.h

--- log/log.h
+++
@@ -1,3 +0,0 @@
-// Copyright (c) 2017 The Authors. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file, See the AUTHORS file for names of contributors.
